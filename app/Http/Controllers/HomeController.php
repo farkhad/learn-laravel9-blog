ce90585393eb2a61507cf8af3d81b08ebba02c18
farkhad <farkhad@gmail.com>
2022-08-14 15:39:33 +0600

    Lesson 7 - second model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = Category::all();
+        $categories = Category::all();
+        $posts = Post::when(request('category_id'), function ($query) {
+            $query->where('category_id', request('category_id'));
+        })
+        ->latest()
+        ->get();
 
-        return view('index', ['categories' => $allCategories]);
+        return view('index', compact('categories', 'posts'));
     }
 }
